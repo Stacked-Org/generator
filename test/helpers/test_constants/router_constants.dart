64d87e58e61f5e6633968c703b3786b3036f1053
devRaphe <etiabasiezekiel@ymail.com>
2023-04-03 16:55:12 +0100

    removed uncessary import

--- test/helpers/test_constants/router_constants.dart
+++ test/helpers/test_constants/router_constants.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:collection/collection.dart';
-
 const kRouterWithNamePathNameClassName = '''
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:flutter/material.dart';
