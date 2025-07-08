1540dc97b608920bc578de11291155f9ee5c4bf4
Sailesh Dahal <saileshbro@gmail.com>
2025-07-08 14:14:28 +0545

    refactor(logging): streamline log handling in SimpleLogPrinter
    - Refactored the instantiation of PrettyPrinter in SimpleLogPrinter to enhance null safety and maintainability.
    - Improved code readability by cleaning up whitespace and formatting in logger constants.
    - Ensured consistent log exclusion logic with clearer conditional statements.

--- test/helpers/test_constants/logger_constants.dart
+++ test/helpers/test_constants/logger_constants.dart
@@ -28,9 +28,13 @@ class SimpleLogPrinter extends LogPrinter {
     this.showOnlyClass,
   });
 
+
+  final printer = PrettyPrinter(
+    levelColors: PrettyPrinter.defaultLevelColors,
+    levelEmojis: PrettyPrinter.defaultLevelEmojis,
+  );
   @override
   List<String> log(LogEvent event) {
-    var printer = PrettyPrinter();
     var color = printer.levelColors?[event.level];
     var emoji = printer.levelEmojis?[event.level];
     var methodName = _getMethodName();
@@ -41,9 +45,12 @@ class SimpleLogPrinter extends LogPrinter {
     var output =
         '\$emoji \$className\$methodNameSection - \${event.message}\${event.error != null ? '\\nERROR: \${event.error}\\n' : ''}\${printCallStack ? '\\nSTACKTRACE:\\n\$stackLog' : ''}';
 
-    if (exludeLogsFromClasses
-            .any((excludeClass) => className == excludeClass) ||
-        (showOnlyClass != null && className != showOnlyClass)) return [];
+    if (exludeLogsFromClasses.any(
+          (excludeClass) => className == excludeClass,
+        ) ||
+        (showOnlyClass != null && className != showOnlyClass)) {
+      return [];
+    }
 
     final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
     List<String> result = [];
